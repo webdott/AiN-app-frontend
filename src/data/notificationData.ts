1cda3a7d670e251f754e02d639f3bec2af859eeb
caspero-62 <nwaforuchechukwu2007@gmail.com>
2021-11-13 14:03:23 +0100

    header fixes

--- src/data/notificationData.ts
+++ src/data/notificationData.ts
@@ -17,6 +17,42 @@ const notificationData = [
         title: 'applied for a GTa role in Egypt',
         message: 'English teacher at Peak Memorial Egypt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
     },
+    {
+        id: '3',
+        name: 'Adeyemi Precious',
+        title: 'applied for a GTa role in Benin',
+        message: 'English teacher at Peak Memorial Benin. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+        id: '4',
+        name: 'Timothy Ogundipe',
+        title: 'confirmed for a GTe role in Lome, Togo',
+        message: 'English teacher at Peak Memorial Lome, Togo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+        id: '5',
+        name: 'Idowu Fakomi',
+        title: 'applied for a GTa role in Egypt',
+        message: 'English teacher at Peak Memorial Egypt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+        id: '6',
+        name: 'Adeyemi Precious',
+        title: 'applied for a GTa role in Benin',
+        message: 'English teacher at Peak Memorial Benin. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+        id: '7',
+        name: 'Timothy Ogundipe',
+        title: 'confirmed for a GTe role in Lome, Togo',
+        message: 'English teacher at Peak Memorial Lome, Togo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+    {
+        id: '8',
+        name: 'Idowu Fakomi',
+        title: 'applied for a GTa role in Egypt',
+        message: 'English teacher at Peak Memorial Egypt. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
 ];
 
 export default notificationData;
