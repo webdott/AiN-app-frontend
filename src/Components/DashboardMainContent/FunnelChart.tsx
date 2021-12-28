78ce7b654e2bbdc2093253379c3d3400c3f8cf35
caspero-62 <nwaforuchechukwu2007@gmail.com>
2021-12-28 13:06:15 +0100

    dashboard page done

--- src/Components/DashboardMainContent/FunnelChart.tsx
+++ src/Components/DashboardMainContent/FunnelChart.tsx
@@ -1,11 +1,102 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
+import { BarChart, Bar, Tooltip, LabelList } from 'recharts';
+
+const data = [
+	{
+		name: 'Signups',
+		value: 110,
+	},
+	{
+		name: 'Applied',
+		value: 72,
+	},
+	{
+		name: 'Accepted',
+		value: 43,
+	},
+	{
+		name: 'Approved',
+		value: 15,
+	},
+	{
+		name: 'Realized',
+		value: 12,
+	},
+	{
+		name: 'Finished',
+		value: 5,
+	},
+	{
+		name: 'Completed',
+		value: 2,
+	},
+];
+
+const renderCustomizedLabel = (props: any) => {
+	const { x, y, width, value } = props;
+	const radius = 16;
+
+	return (
+		<g>
+			<text
+				x={x + width / 2}
+				y={y - radius}
+				fill='#52565E'
+				textAnchor='middle'
+				dominantBaseline='middle'
+				fontWeight='300'
+				fontSize='14'
+			>
+				{value}
+			</text>
+		</g>
+	);
+};
+
+const renderCustomizedLabelBottom = (props: any) => {
+	const { x, width, value } = props;
+
+	return (
+		<g>
+			<text
+				x={x + width / 2}
+				y={410}
+				fill='#52565E'
+				textAnchor='middle'
+				dominantBaseline='middle'
+				fontWeight='300'
+				fontSize='9'
+			>
+				{value}
+			</text>
+		</g>
+	);
+};
 
 const FunnelChart: FC = () => {
-    return (
-        <div className='chart funnel__chart'>
-            
-        </div>
-    );
+	return (
+		<div className='chart funnel__chart'>
+			<p className='title'>Funnel chart</p>
+
+			<BarChart
+				width={494}
+				height={416}
+				data={data}
+				margin={{
+					top: 5,
+					right: 0,
+					left: 0,
+					bottom: 20,
+				}}
+			>
+				<Tooltip />
+				<Bar dataKey='value' fill='#037EF3' width={56}>
+					<LabelList dataKey='value' content={renderCustomizedLabel} />
+					<LabelList dataKey='name' content={renderCustomizedLabelBottom} />
+				</Bar>
+			</BarChart>
+		</div>
+	);
 };
 
 export default FunnelChart;
